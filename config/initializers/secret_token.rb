2bc0629072664db880724438f95ff2c62318d014
Antonio Chavez <cavjzz@gmail.com>
2012-03-14 09:35:05 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alzheimer::Application.config.secret_token = '70b3b176d3f2e5a2a89d2385260c9bd8f16ba31c29e4fd2aeb73dc3626ff065a63c55de39059f7df4dd282389915c40955ba008ac9ea4857bce86d916b58f253'
