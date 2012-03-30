3117ddecef91e80d8ff84fadf7334b1a622b1317
Antonio Chavez <cavjzz@gmail.com>
2012-03-29 18:35:16 -0600

    Added question model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120328175537) do
+ActiveRecord::Schema.define(:version => 20120330003418) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -103,6 +103,13 @@ ActiveRecord::Schema.define(:version => 20120328175537) do
     t.string   "city"
     t.integer  "number"
     t.string   "state"
+  end
+
+  create_table "questions", :force => true do |t|
+    t.string   "question"
+    t.string   "answer"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
