e0296dde63b52f36ee5b9980259983ccdb543581
Antonio Chavez <cavjzz@gmail.com>
2012-04-17 17:24:00 -0500

    CChanged admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if application_admin_user_signed_in?
+      "admin"
+    else
+      "application"
+    end
+  end
 end
