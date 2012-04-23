c2121946c3df09c0975b5681bfe22a1742331eea
Antonio Chavez <cavjzz@gmail.com>
2012-04-23 09:56:09 -0500

    Changed gnosia migration

--- db/migrate/20120423145421_create_gnosia.rb
+++ db/migrate/20120423145421_create_gnosia.rb
@@ -1,7 +1,8 @@
 class CreateGnosia < ActiveRecord::Migration
   def change
     create_table :gnosia do |t|
-
+      t.string :name
+      t.has_attached_file :image
       t.timestamps
     end
   end
