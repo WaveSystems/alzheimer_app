2a97b49d2a3b22c1a902544027e1b89d9651d4b2
Antonio Chavez <cavjzz@gmail.com>
2012-04-23 10:05:20 -0500

    Modified gnosia image upload

--- app/admin/gnosia.rb
+++ app/admin/gnosia.rb
@@ -1,3 +1,11 @@
-ActiveAdmin.register Gnosium do
+ActiveAdmin.register Gnosia do
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :image, :as => :file
+    end
+    f.buttons
+  end
   
 end
