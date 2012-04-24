d7bc6769973b3bd4101547b06b5cfffd7beb9813
Antonio Chavez <cavjzz@gmail.com>
2012-04-24 09:55:11 -0500

    Customized image uploading and thumbnails for memoria model

--- app/admin/memoria.rb
+++ app/admin/memoria.rb
@@ -1,3 +1,16 @@
-ActiveAdmin.register Memorium do
+ActiveAdmin.register Memoria do
+  
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :answer
+      f.input :image, :as => :file
+    end
+    f.buttons
+  end
+
+  index :as => :grid do |memoria|
+    link_to(image_tag(memoria.image.url(:medium)))
+  end
   
 end
--- app/models/memoria.rb
+++ app/models/memoria.rb
@@ -1,3 +1,3 @@
 class Memoria < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "210x141>"}
+  has_attached_file :image, :styles => { :small => "210x141>", :medium => "700x500>"}
 end
