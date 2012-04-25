cb63f536288bea604f8ca4e1046a4aaa0f5e2401
Antonio Chavez <cavjzz@gmail.com>
2012-04-25 12:23:02 -0500

    Added new table events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120424144729) do
+ActiveRecord::Schema.define(:version => 20120425171903) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -63,6 +63,14 @@ ActiveRecord::Schema.define(:version => 20120424144729) do
 
   add_index "application_admin_users", ["email"], :name => "index_application_admin_users_on_email", :unique => true
   add_index "application_admin_users", ["reset_password_token"], :name => "index_application_admin_users_on_reset_password_token", :unique => true
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "gnosia", :force => true do |t|
     t.string   "name"
