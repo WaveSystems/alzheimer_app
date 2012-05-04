79fdb343c7d407bddaa5abcd5b805e16e7a7eac7
Antonio Chavez <cavjzz@gmail.com>
2012-05-04 09:27:28 -0500

    Added new functionality to calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -8,5 +8,23 @@ class CalendarController < ApplicationController
 
     @event_strips = Event.event_strips_for_month(@shown_month)
   end
-  
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params[:event])
+    if @event.save
+      flash[:notice] = "Evento guardado exitosamente"
+      redirect_to :calendar
+    else
+      flash[:alert] = "Ups! Hubo un error al guardar su informacion, intente nuevamente..."
+      redirect_to :new_event
+    end
+  end
+
+  def show
+    @event = Event.find(params[:event_id])
+  end
 end
