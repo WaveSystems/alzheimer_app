06e391f1bbbc3f9f00656b6ef243164ae0a64ee5
Antonio Chavez <cavjzz@gmail.com>
2012-05-09 02:31:01 -0500

    Added gnosia field to back and frontend

--- app/admin/gnosia.rb
+++ app/admin/gnosia.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register Gnosia do
   form do |f|
     f.inputs do
       f.input :name
+      f.input :question
       f.input :answer_raw, :label => 'Answer'
       f.input :image, :as => :file
     end
