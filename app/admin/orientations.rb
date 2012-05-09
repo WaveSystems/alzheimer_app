9ca39968b9d9a5a9bfeed540172c6ad3e42ff566
Antonio Chavez <cavjzz@gmail.com>
2012-05-09 03:01:37 -0500

    Added right fields for models

--- app/admin/orientations.rb
+++ app/admin/orientations.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Orientation do
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :image, :as => :file
+    end
+    f.buttons
+  end
+
+  index :as => :grid do |orientation|
+    link_to(image_tag(orientation.image.url(:medium)))
+  end
   
 end
--- app/admin/praxia.rb
+++ app/admin/praxia.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Praxia do
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :image, :as => :file
+    end
+    f.buttons
+  end
+
+  index :as => :grid do |praxia|
+    link_to(image_tag(praxia.image.url(:medium)))
+  end
   
 end
