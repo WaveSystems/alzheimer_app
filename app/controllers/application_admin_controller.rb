0e625a1493c4add48e2fb53c842185249042b313
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2012-12-05 23:45:27 -0600

    Refactored controllers

--- app/controllers/application_admin_controller.rb
+++ app/controllers/application_admin_controller.rb
@@ -1,12 +1,4 @@
-class ApplicationAdminController < ApplicationController
+class ApplicationAdminController < ActionController::Base
+  layout 'admin'
   before_filter :authenticate_application_admin_user!
-  before_filter :redirect_to_admin_index
-  skip_before_filter :authenticate_user!
-  
-  private
-  def redirect_to_admin_index
-    if application_admin_user_signed_in?
-      redirect_to application_admin_organizations_path
-    end
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout :layout_by_resource
   before_filter :authenticate_user!
-
-  private
-
-  def layout_by_resource
-    if application_admin_user_signed_in?
-      "admin"
-    else
-      "application"
-    end
-  end
 end
