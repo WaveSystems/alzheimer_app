64f509877df3bfcd8ef5964e6d4eb858c448d229
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2012-12-10 23:54:13 -0600

    Finally rendered template by category

--- app/assets/javascripts/app/views/exerciseItem.js
+++ app/assets/javascripts/app/views/exerciseItem.js
@@ -1,7 +1,12 @@
 AA.Views.exerciseItem = Backbone.View.extend({
   el: '.fn-render-exercise-templates',
 
-  initialize: function(){
-    this.template =
+  initialize: function(templateName){
+    this.template = Mustache.to_html($('.fn-' + templateName).html(), { message: 'Hello from ' + templateName });
+    this.render(); 
+  },
+
+  render: function(){
+    $(this.el).html(this.template);
   }
 });
--- app/assets/javascripts/app/views/exercises.js
+++ app/assets/javascripts/app/views/exercises.js
@@ -13,19 +13,25 @@ AA.Views.exercises = Backbone.View.extend({
   },
 
   renderExerciseItem: function(className){
+    var exerciseItem;
     switch (className) {
       case "btn fn-category calculus active":
-
+        exerciseItem = new AA.Views.exerciseItem('calculus');
         break;
       case "btn fn-category gnosias active":
+        exerciseItem = new AA.Views.exerciseItem('gnosias');
         break;
       case "btn fn-category memory active":
+        exerciseItem = new AA.Views.exerciseItem('memory');
         break;
       case "btn fn-category language active":
+        exerciseItem = new AA.Views.exerciseItem('language');
         break;
       case "btn fn-category orientation active":
+        exerciseItem = new AA.Views.exerciseItem('orientation');
         break;
       case "btn fn-category praxias active":
+        exerciseItem = new AA.Views.exerciseItem('praxias');
         break;
       default:
         break;
