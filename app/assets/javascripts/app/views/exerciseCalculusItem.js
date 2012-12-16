6c12a479bcb269fba4f73174dc0222bd0637897e
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2012-12-15 19:19:12 -0600

    Added new jquery events

--- app/assets/javascripts/app/views/exerciseCalculusItem.js
+++ app/assets/javascripts/app/views/exerciseCalculusItem.js
@@ -2,10 +2,16 @@ AA.Views.exerciseCalculusItem = Backbone.View.extend({
   el: '.fn-item-calculus',
 
   events: {
-    'click .fn-send' : 'hideAndScore'
+    'click .fn-send'    : 'hideAndScore',
+    'keypress .fn-answer' : 'hideAndScore'
   },
 
-  hideAndScore: function(){
+  hideAndScore: function(e){
+    if (e.keyCode == 13 || e.type == "click"){
+      var answer = $('.fn-answer').val();
+      console.log(answer);
+      $('.fn-answer').val('');
+    }
   }
 
 });
