8f20878ea2ec8f7485ef6089879b924039780a46
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2012-12-15 19:00:14 -0600

    Rendered new template items

--- app/assets/javascripts/app/views/exerciseCalculus.js
+++ app/assets/javascripts/app/views/exerciseCalculus.js
@@ -2,22 +2,28 @@ AA.Views.exerciseCalculus = Backbone.View.extend({
   el: '.fn-render-exercise-templates',
 
   events: {
-    'click .fn-submit' : 'hideAndEvaluate'
   },
 
   initialize: function(){
     this.templateName = 'calculus';
+    this.templateItemName = 'item-calculus-template';
     this.collection = new AA.Collections.calculus();
     this.collection.bind('reset change', this.render, this); 
     this.collection.fetch();
   },
 
   render: function(){
-    this.template = Mustache.to_html($('.fn-' + this.templateName).html(), { questions: this.collection.toJSON() });
-    this.questionsCount = this.collection.toJSON().length
+    this.template = Mustache.to_html($('.fn-' + this.templateName).html());
     $(this.el).html(this.template);
+    this.renderItems();
   },
 
-  hideAndEvaluate: function(e){
+  renderItems: function(){
+    var self = this;
+    _.each(this.collection.toJSON(), function(question){
+      var templateItem = Mustache.to_html($('.fn-' + self.templateItemName).html(), {question: question});
+      $('.fn-body-table').append(templateItem);
+    });
+    exerciseItem = new AA.Views.exerciseCalculusItem();
   }
 });
---
+++ app/assets/javascripts/app/views/exerciseCalculusItem.js
@@ -0,0 +1,11 @@
+AA.Views.exerciseCalculusItem = Backbone.View.extend({
+  el: '.fn-item-calculus',
+
+  events: {
+    'click .fn-send' : 'hideAndScore'
+  },
+
+  hideAndScore: function(){
+  }
+
+});
